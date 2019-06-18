d9d638891ca8395f0610cb8fafabc26900f746ff
zj-work <zj_work01@outlook.com>
2019-06-18 09:43:14 +0800

    扩展项目添加

---
+++ D.Extend/Encryption.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Extend
+{
+    public class Encryption
+    {
+        #region MD5加密
+
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="content">需要加密的字符串</param>
+        /// <returns></returns>
+        public string MD5Encrypt(string content)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] bts = Encoding.UTF8.GetBytes(content);
+            byte[] encryData = md5.ComputeHash(bts);
+            return Encoding.UTF8.GetString(encryData);
+        }
+
+        #endregion
+
+        #region AES加密解密
+
+        /// <summary>
+        /// AES加密 
+        /// </summary>
+        /// <param name="text">需要的加密字符</param>
+        /// <param name="password">加密的密码</param>
+        /// <param name="iv">密钥</param>
+        /// <returns></returns>
+        public static string AESEncrypt(string text, string password, string iv)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+            rijndaelCipher.KeySize = 128;
+            rijndaelCipher.BlockSize = 128;
+            byte[] pwdBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] keyBytes = new byte[16];
+            int len = pwdBytes.Length;
+            if (len > keyBytes.Length) len = keyBytes.Length;
+            System.Array.Copy(pwdBytes, keyBytes, len);
+            rijndaelCipher.Key = keyBytes;
+            byte[] ivBytes = System.Text.Encoding.UTF8.GetBytes(iv);
+            rijndaelCipher.IV = new byte[16];
+            ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
+            byte[] plainText = Encoding.UTF8.GetBytes(text);
+            byte[] cipherBytes = transform.TransformFinalBlock(plainText, 0, plainText.Length);
+            return Convert.ToBase64String(cipherBytes);
+        }
+
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="text">加密后的字符串</param>
+        /// <param name="password">加密的密码</param>
+        /// <param name="iv">秘钥</param>
+        /// <returns></returns>
+        public static string AESDecrypt(string text, string password, string iv)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+            rijndaelCipher.KeySize = 128;
+            rijndaelCipher.BlockSize = 128;
+            byte[] encryptedData = Convert.FromBase64String(text);
+            byte[] pwdBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] keyBytes = new byte[16];
+            int len = pwdBytes.Length;
+            if (len > keyBytes.Length) len = keyBytes.Length;
+            System.Array.Copy(pwdBytes, keyBytes, len);
+            rijndaelCipher.Key = keyBytes;
+            byte[] ivBytes = System.Text.Encoding.UTF8.GetBytes(iv);
+            rijndaelCipher.IV = ivBytes;
+            ICryptoTransform transform = rijndaelCipher.CreateDecryptor();
+            byte[] plainText = transform.TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+            return Encoding.UTF8.GetString(plainText);
+        }
+
+
+        #endregion
+
+        #region SHA1加密
+
+        /// <summary>
+        /// SHA1加密方法
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string SHA1Encrypt(string input)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            byte[] result = sha.ComputeHash(inputBytes);
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                sBuilder.Append(result[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        #endregion
+
+        #region SHA256加密
+
+        /// <summary>
+        /// SHA256加密方法
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string SHA256Encrypt(string data)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            byte[] hash = SHA256Managed.Create().ComputeHash(bytes);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                builder.Append(hash[i].ToString("X2"));
+            }
+
+            return builder.ToString();
+        }
+
+        #endregion
+
+        #region Base64加密解密
+
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string Base64Encrypt(string data)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string Base64Decrypt(string data)
+        {
+            byte[] bytes = Convert.FromBase64String(data);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        #endregion
+
+        #region DES加密解密
+
+        /// <summary>  
+        /// C# DES解密方法  
+        /// </summary>  
+        /// <param name="encryptedValue">待解密的字符串</param>  
+        /// <param name="key">密钥</param>  
+        /// <param name="iv">向量</param>  
+        /// <returns>解密后的字符串</returns>  
+        public string DESDecrypt(string encryptedValue, string key, string iv)
+        {
+            using (DESCryptoServiceProvider sa =
+                new DESCryptoServiceProvider
+                { Key = Encoding.UTF8.GetBytes(key), IV = Encoding.UTF8.GetBytes(iv) })
+            {
+                using (ICryptoTransform ct = sa.CreateDecryptor())
+                {
+                    byte[] byt = Convert.FromBase64String(encryptedValue);
+
+                    using (var ms = new MemoryStream())
+                    {
+                        using (var cs = new CryptoStream(ms, ct, CryptoStreamMode.Write))
+                        {
+                            cs.Write(byt, 0, byt.Length);
+                            cs.FlushFinalBlock();
+                        }
+                        return Encoding.UTF8.GetString(ms.ToArray());
+                    }
+                }
+            }
+        }
+
+        /// <summary>  
+        /// C# DES加密方法  
+        /// </summary>  
+        /// <param name="encryptedValue">要加密的字符串</param>  
+        /// <param name="key">密钥</param>  
+        /// <param name="iv">向量</param>  
+        /// <returns>加密后的字符串</returns>  
+        public static string DESEncrypt(string originalValue, string key, string iv)
+        {
+            using (DESCryptoServiceProvider sa
+                = new DESCryptoServiceProvider { Key = Encoding.UTF8.GetBytes(key), IV = Encoding.UTF8.GetBytes(iv) })
+            {
+                using (ICryptoTransform ct = sa.CreateEncryptor())
+                {
+                    byte[] by = Encoding.UTF8.GetBytes(originalValue);
+                    using (var ms = new MemoryStream())
+                    {
+                        using (var cs = new CryptoStream(ms, ct,CryptoStreamMode.Write))
+                        {
+                            cs.Write(by, 0, by.Length);
+                            cs.FlushFinalBlock();
+                        }
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
