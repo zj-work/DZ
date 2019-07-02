a7a356c49475fb6d89503e2e7f1e8bcaa6786656
zj-work <zj_work01@outlook.com>
2019-07-02 23:28:59 +0800

    图片转换工具

---
+++ Z.Tackle/MainForm.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Z.Tackle
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.fl_top = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.fl_top.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fl_top
+            // 
+            this.fl_top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.fl_top.Controls.Add(this.pictureBox1);
+            this.fl_top.Controls.Add(this.label1);
+            this.fl_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.fl_top.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.fl_top.Location = new System.Drawing.Point(0, 0);
+            this.fl_top.Name = "fl_top";
+            this.fl_top.Size = new System.Drawing.Size(290, 32);
+            this.fl_top.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(258, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(78, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 5, 55, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(122, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "图片格式转换器";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(290, 183);
+            this.panel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "开始转换";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 52);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(290, 58);
+            this.panel3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "请选择要转换的格式";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".bmp",
+            ".jpg",
+            ".png"});
+            this.comboBox1.Location = new System.Drawing.Point(122, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(290, 52);
+            this.panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.Location = new System.Drawing.Point(12, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(233, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 160);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(290, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 215);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.fl_top);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "图片格式转换工具";
+            this.fl_top.ResumeLayout(false);
+            this.fl_top.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel fl_top;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Z.Tackle/MainForm.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Z.Tackle
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.fl_top.MouseMove += MainForm_MouseMove;
+            this.fl_top.MouseDown += MainForm_MouseDown;
+            this.label1.MouseMove += MainForm_MouseMove;
+            this.label1.MouseDown += MainForm_MouseDown;
+
+            this.textBox1.Text = "请选择文件夹路径";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.comboBox1.SelectedIndex = 0;
+            this._extend = this.comboBox1.SelectedItem.ToString();
+            button2.Focus();
+            progressBar1.Minimum = 0;
+        }
+
+        private bool _isDirty = false;//是否正在解析
+        private string _folder = string.Empty;//用户选择的文件夹路径
+        private string _extend = ".bmp";//转换后的文件格式
+
+        #region Form methods
+
+        private Point _mousePoint;
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _mousePoint.X = e.X;
+                _mousePoint.Y = e.Y;
+            }
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Top = MousePosition.Y - _mousePoint.Y;
+                Left = MousePosition.X - _mousePoint.X;
+            }
+        }
+
+
+        #endregion
+
+        #region Event methods
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (this._isDirty)
+            {
+                if (MessageBox.Show("", "温馨提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void Select_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this._folder = dialog.SelectedPath;
+                this.textBox1.Text = this._folder;
+                this.toolTip1.SetToolTip(this.textBox1, this.textBox1.Text);
+            }
+            dialog.Dispose();
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            this._extend = cb.SelectedItem.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            CommandTackle();
+            button2.Enabled = true;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 图片格式转换
+        /// </summary>
+        public async void CommandTackle()
+        {
+            progressBar1.Visible = true;
+            if (string.IsNullOrEmpty(this._folder))
+            {
+                MessageBox.Show("请选择文件夹路径", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (string.IsNullOrEmpty(this._extend))
+            {
+                MessageBox.Show("请选择转换格式", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            this._isDirty = true;
+
+            List<Task> tasks = new List<Task>();
+            List<FileInfo> files = new List<FileInfo>();//选择文件夹以及所有子文件夹下的文件
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(this._folder);
+            while(queue.Count > 0)
+            {
+                var currentfolder = queue.Dequeue();
+                DirectoryInfo dir = new DirectoryInfo(currentfolder);
+                //添加当前文件夹下的图片文件
+                files.AddRange(dir.GetFiles());
+                //添加当前文件夹下的子文件夹
+                foreach(var item in dir.GetDirectories())
+                {
+                    queue.Enqueue(item.FullName);
+                }
+            }
+            progressBar1.Value = 0;
+            progressBar1.Maximum = files.Count;
+            foreach (var file in files)
+            {
+                tasks.Add(Task.Factory.StartNew((obj) =>
+                {
+                    try
+                    {
+                        var filePath = obj.ToString();
+                        var extend = Path.GetExtension(filePath);
+                        if (!this._extend.Equals(extend) && IsImage(extend))
+                        {//将不是指定格式的文件转换为指定格式的文件
+                            var newFilePath = filePath.Replace(extend, this._extend);
+                            Bitmap bmp = new Bitmap(filePath);
+                            bmp.Save(newFilePath);
+                        }
+                        progressBar1.Value += 1;
+                    }
+                    catch { }
+                }, file.FullName, System.Threading.CancellationToken.None));
+            }
+            await Task.WhenAll(tasks);
+            progressBar1.Value = progressBar1.Maximum;
+            progressBar1.Visible = false;
+            this._isDirty = false;
+            MessageBox.Show("转换完成", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool IsImage(string extension)
+        {
+            var imageExtension = new string[]
+            {
+                ".bmp",".tiff",".jpg",".png"
+            };
+            return imageExtension.Contains(extension);
+        }
+
+        #endregion
+
+    }
+}
